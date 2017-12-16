892ae1230f412dd5966b4bdddda3bce987255220
Bryan Finlayson <bryan.finlayson@metova.com>
2017-12-16 14:02:14 -0600

    add shared_example for SirenClient::Field

--- spec/unit/link_spec.rb
+++ spec/unit/link_spec.rb
@@ -1,51 +1,70 @@
 require 'helper/spec_helper'
 
 describe SirenClient::Link do
-  let (:link_data) { {"rel"=>["self"],"href"=>"http://example.com/products/03283378000P"} }
+  let (:link_data) { {"rel"=>["self"],"href"=>"http://example.com/products/03283378000P", "title"=>"test", "type"=>"test"} }
+
+  subject { SirenClient::Link }
 
   describe '.new(data)' do
-      it 'raise an error if wrong type is provided' do
-        expect { SirenClient::Link.new([]) }.to raise_error(ArgumentError)
-      end
-      it 'can be instanciated with a hash' do
-        expect(SirenClient::Link.new(link_data)).to be_a SirenClient::Link
-      end
+    it 'raise an error if wrong type is provided' do
+      expect { subject.new([]) }.to raise_error(ArgumentError)
+    end
+
+    it 'can be instanciated with a hash' do
+      expect(subject.new(link_data)).to be_a SirenClient::Link
+    end
   end
 
-  let (:link) { SirenClient::Link.new(link_data) }
-  describe '.go' do
-    it 'follows the link\'s href' do
-      # We just need to know that the link will make the request.
-      expect { link.go }.to raise_error(SirenClient::InvalidResponseError)
+  describe 'a SirenClient::Link instance' do
+    context 'when initialized with stringified keys' do
+      let (:link) { subject.new(link_data) }
+
+      it_behaves_like 'a SirenClient::Link'
+    end
+
+    context 'when initialized with symbolized keys' do
+      let (:link) { subject.new(link_data.deep_symbolize_keys) }
+
+      it_behaves_like 'a SirenClient::Link'
     end
   end
-  describe '.payload' do
-    it 'is a hash' do
-      expect(link.payload).to be_a Hash
+
+  describe 'when initialized with no data' do
+    let (:link) { subject.new({}) }
+
+    describe '.payload' do
+      it 'is a hash' do
+        expect(link.payload).to be_a Hash
+      end
     end
-  end
-  describe '.rels' do
-    it 'is an array' do
-      expect(link.rels).to be_a Array
+
+    describe '.rels' do
+      it 'is an array' do
+        expect(link.rels).to be_a Array
+      end
     end
-  end
-  describe '.href' do
-    it 'is a string' do
-      expect(link.href).to be_a String
-    end
-    it 'can change .href as needed' do
+
+    describe '.href' do
+      it 'is a string' do
+        expect(link.href).to be_a String
+      end
+
+      it 'can change .href as needed' do
         link.href = link.href + '?query=test'
         expect(/query=test/).to match(link.href)
+      end
     end
-  end
-  describe '.title' do
-    it 'is a string' do
-      expect(link.title).to be_a String
+
+    describe '.title' do
+      it 'is a string' do
+        expect(link.title).to be_a String
+      end
     end
-  end
-  describe '.type' do
-    it 'is a string' do
-      expect(link.type).to be_a String
+
+    describe '.type' do
+      it 'is a string' do
+        expect(link.type).to be_a String
+      end
     end
   end
 end
